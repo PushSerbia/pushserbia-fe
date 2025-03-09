b077b356437719c75b698046fe4e16127d8caefb
Dusan Perisic <dullle@live.com>
2025-03-09 13:44:02 +0100

    feat: implement email subscription feature with loading state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,33 @@
-import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit, OnDestroy {
   days = 0;
   hours = 0;
   minutes = 0;
   seconds = 0;
+  emailCtrl = new FormControl('', [Validators.required, Validators.email]);
+  savingInProgress = signal(false);
+  emailSent = signal(false);
 
   private countdownInterval: any;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private httpClient: HttpClient,
+  ) {
   }
 
   ngOnInit(): void {
@@ -25,6 +37,25 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     clearInterval(this.countdownInterval);
+  }
+
+  onNotifyMeSubmitted(): void {
+    if (this.savingInProgress()) {
+      return;
+    }
+    if (this.emailCtrl.invalid) {
+      this.emailCtrl.markAllAsTouched();
+      return;
+    }
+
+    // todo: move it to the independent service
+    // todo: make environment configurations
+    this.savingInProgress.set(true)
+    this.httpClient.post('https://api.pushserbia.com/v1/integrations/subscribe', { email: this.emailCtrl.value, tags: 'coming-soon' }).pipe(finalize(() => {
+      this.savingInProgress.set(false)
+    })).subscribe(() => {
+      this.emailSent.set(true);
+    });
   }
 
   private startCountdown(): void {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,12 +9,14 @@ import { provideRouter, Router } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import * as Sentry from "@sentry/angular";
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
     {
       provide: ErrorHandler,
       useValue: Sentry.createErrorHandler(),
