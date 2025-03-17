5b9c0439ad2f70e035505bdb6ada314098f8ad4d
grizlizli <mkrstic@capaciteam.com>
2025-03-17 10:30:08 +0100

    generate pages

---
+++ src/app/projects/pages/projects-list-page/projects-list-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-list-page',
+  imports: [],
+  templateUrl: './projects-list-page.component.html',
+  styleUrl: './projects-list-page.component.scss'
+})
+export class ProjectsListPageComponent {
+
+}
