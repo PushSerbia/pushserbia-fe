c0ed3b5ec5fba9116149ea6ccbf8b8706a67ccd9
Dusan Perisic <dullle@live.com>
2025-03-20 23:04:40 +0100

    feat: add user widget component for user authentication and profile management #9

--- src/app/features/landing/sections/landing-header/landing-header.component.ts
+++ src/app/features/landing/sections/landing-header/landing-header.component.ts
@@ -1,20 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { AuthService } from '../../../../core/auth/auth.service';
-import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { UserWidgetComponent } from './widgets/user-widget/user-widget.component';
 
 @Component({
   selector: 'app-landing-header',
-  imports: [AsyncPipe, RouterLink],
+  imports: [UserWidgetComponent],
   templateUrl: './landing-header.component.html',
   styleUrl: './landing-header.component.css'
 })
 export class LandingHeaderComponent {
-  private authService = inject(AuthService);
-
-  user$ = this.authService.userData$;
-
-  onLogoutClick(): void {
-    this.authService.signOut().subscribe();
-  }
 }
