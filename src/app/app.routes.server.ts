6c697502a4732b2d34d6e29dcbda3718ff436cc1
Dusan Perisic <dullle@live.com>
2025-03-26 18:35:32 +0100

    fix: temporary remove route prerender to avoid build issues

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,18 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+// todo: check and improve config
+
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'admin/**',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'projects/**',
+    renderMode: RenderMode.Client,
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
 ];
