333562691524b8ca339f03d25d8b0cb0ec385566
Dusan Perisic <dullle@live.com>
2025-03-28 18:17:34 +0100

    feat: add ProjectCardNew component with template, styles, and initial tests #19

---
+++ src/app/shared/ui/project-card-new/project-card-new.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectCardNewComponent } from './project-card-new.component';
+
+describe('ProjectCardNewComponent', () => {
+  let component: ProjectCardNewComponent;
+  let fixture: ComponentFixture<ProjectCardNewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProjectCardNewComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProjectCardNewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/ui/project-card-new/project-card-new.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-project-card-new',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './project-card-new.component.html',
+  styleUrl: './project-card-new.component.css'
+})
+export class ProjectCardNewComponent {
+
+}
