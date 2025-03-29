8b92ce4cb245f37317282eeda38e22e44d9d42ee
Dusan Perisic <dullle@live.com>
2025-03-29 15:51:00 +0100

    feat: add profile information dialog component with form for LinkedIn, GitHub, and biography #16

---
+++ src/app/features/profile/pages/profile-page/components/profile-information-dialog/profile-information-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfileInformationDialogComponent } from './profile-information-dialog.component';
+
+describe('ProfileInformationDialogComponent', () => {
+  let component: ProfileInformationDialogComponent;
+  let fixture: ComponentFixture<ProfileInformationDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProfileInformationDialogComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProfileInformationDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
