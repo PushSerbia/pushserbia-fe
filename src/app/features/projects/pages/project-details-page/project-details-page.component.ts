af8a287460429a883f5fc013260c505a1f7552b7
grizlizli <mkrstic@capaciteam.com>
2025-03-30 14:43:59 +0200

    project details layout

--- src/app/features/projects/pages/project-details-page/project-details-page.component.ts
+++ src/app/features/projects/pages/project-details-page/project-details-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BasicLayoutComponent } from '../../../../shared/layout/landing-layout/basic-layout.component';
 
 @Component({
   selector: 'app-project-details-page',
-  imports: [],
+  imports: [BasicLayoutComponent],
   templateUrl: './project-details-page.component.html',
   styleUrl: './project-details-page.component.scss'
 })
