23b0768d36edaad22d0a306a311e8eb8dbd7320a
Dusan Perisic <dullle@live.com>
2025-04-04 18:23:41 +0200

    chore: update VoteService to use Vote model and add getMyVotes method

--- src/app/core/vote/vote.service.ts
+++ src/app/core/vote/vote.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api/api.service';
+import { Vote } from './vote';
 
 @Injectable({
   providedIn: 'root'
 })
-export class VoteService extends ApiService<any>{
+export class VoteService extends ApiService<Vote>{
   readonly endpoint = 'votes';
 
-  voteForProject(projectId: string) {
-    return super.set(projectId, {});
+  getMyVotes() {
+    return super.getById<Vote[]>('my-votes');
   }
 }
---
+++ src/app/core/vote/vote.ts
@@ -0,0 +1,5 @@
+export interface Vote {
+  userId: string;
+  projectId: string;
+  createdAt: Date;
+}
