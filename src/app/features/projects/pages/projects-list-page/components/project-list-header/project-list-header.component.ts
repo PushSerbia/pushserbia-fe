50ca3531a2fcca9d0dae0b5b2f3379b6fa838c86
Dusan Perisic <dullle@live.com>
2025-04-11 21:04:05 +0200

    feat: add project list header component with template, styles, and tests

---
+++ src/app/features/projects/pages/projects-list-page/components/project-list-header/project-list-header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-project-list-header',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './project-list-header.component.html',
+  styleUrl: './project-list-header.component.css'
+})
+export class ProjectListHeaderComponent {
+
+}
