80000611863e7352340d1c78d356acb42c327b0d
Dusan Perisic <dullle@live.com>
2025-04-11 21:03:34 +0200

    feat: refactor project service and add project store service for state management

--- src/app/core/project/project.service.ts
+++ src/app/core/project/project.service.ts
@@ -1,37 +1,10 @@
-import { Injectable, Signal } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ApiService } from '../api/api.service';
-import { map, throwError } from 'rxjs';
 import { Project } from './project';
-import { httpResource } from '@angular/common/http';
-import { rxResource } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService extends ApiService<Project>{
   readonly endpoint = 'projects';
-
-  getProjectsResource(page: Signal<number>) {
-    return httpResource<Project[]>(() => `/projects?page=${page()}`);
-  }
-
-  getProjectDetailsResource(slug: Signal<string>) {
-    return rxResource<Project, { slug: string }>({
-      request: () => ({ slug: slug() }),
-      loader: ({ request }) => super.getAll(request)
-        .pipe(
-          map((projects: Project[]) => {
-            if (projects.length === 0) {
-              throw new Error(`No project found with slug '${request.slug}'`);
-            }
-            else {
-              return projects[0];
-            }
-        }))
-    });
-  }
-
-  // override getById<Project>(id: string) {
-  //   return super.getAll({ slug: id }).pipe(map(items => items[0] as Project));
-  // }
 }
