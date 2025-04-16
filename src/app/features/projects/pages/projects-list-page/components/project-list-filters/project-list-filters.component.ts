22e59aec84a1adccd5969795e279e8c2bfb5f65d
Dusan Perisic <dullle@live.com>
2025-04-16 21:33:28 +0200

    feat: update project list filters to emit changes for my projects only #40

--- src/app/features/projects/pages/projects-list-page/components/project-list-filters/project-list-filters.component.ts
+++ src/app/features/projects/pages/projects-list-page/components/project-list-filters/project-list-filters.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { ProjectsFilter } from '../../../../../../core/project/projects-filter';
 
 @Component({
   selector: 'app-project-list-filters',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './project-list-filters.component.css'
 })
 export class ProjectListFiltersComponent {
-
+  updated = output<ProjectsFilter>();
 }
