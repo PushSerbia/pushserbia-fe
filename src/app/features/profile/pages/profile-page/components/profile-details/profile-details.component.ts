be367638ba086bbadb548a0e70c6361f3738c6aa
Dusan Perisic <dullle@live.com>
2025-05-11 11:50:38 +0200

    feat: integrate modal service for profile details dialog management and remove Flowbite dependency

--- src/app/features/profile/pages/profile-page/components/profile-details/profile-details.component.ts
+++ src/app/features/profile/pages/profile-page/components/profile-details/profile-details.component.ts
@@ -1,10 +1,10 @@
-import { afterRender, Component, input } from '@angular/core';
+import { Component, input, OnDestroy } from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { User } from '../../../../../../core/user/user';
 import { FirebaseUserData } from '../../../../../../core/user/firebase-user-data';
 import { ProfileInformationDialogComponent } from '../profile-information-dialog/profile-information-dialog.component';
-import { initFlowbite } from 'flowbite';
 import { ProfileProCardComponent } from '../profile-pro-card/profile-pro-card.component';
+import { ModalService } from '../../../../../../core/modal/modal.service';
 
 @Component({
   selector: 'app-profile-details',
@@ -16,12 +16,22 @@ import { ProfileProCardComponent } from '../profile-pro-card/profile-pro-card.co
   templateUrl: './profile-details.component.html',
   styleUrl: './profile-details.component.css',
 })
-export class ProfileDetailsComponent {
+export class ProfileDetailsComponent implements OnDestroy {
+  id = 'profile-information-dialog';
   data = input.required<User & FirebaseUserData>();
 
-  constructor() {
-    afterRender(() => {
-      initFlowbite();
-    });
+  constructor(private modal: ModalService) {}
+
+  ngOnDestroy(): void {
+    this.closeProfileInformationDialog();
+    this.modal.remove(this.id);
+  }
+
+  openProfileInformationDialog(): void {
+    this.modal.open(this.id);
+  }
+
+  closeProfileInformationDialog(): void {
+    this.modal.close(this.id);
   }
 }
