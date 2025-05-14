8477e7e7acdceed7b84ba130ada3197a4c9660df
Dusan Perisic <dullle@live.com>
2025-05-14 17:33:38 +0200

    feat: integrate versioned Sentry release and improve build process

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'http://localhost:3000/v1',
   firebase: {
-    projectId: "pushserbia-be79a",
-    appId: "1:245366071118:web:aea6b3a5e48f4d6a71881f",
-    storageBucket: "pushserbia-be79a.firebasestorage.app",
-    apiKey: "AIzaSyCduiH8Da5hvBUn84_Ddl7bNoRmTVSVsQg",
-    authDomain: "pushserbia-be79a.firebaseapp.com",
-    messagingSenderId: "245366071118",
-    measurementId: "G-ZHFBEDQBG5"
-  }
+    projectId: 'pushserbia-be79a',
+    appId: '1:245366071118:web:aea6b3a5e48f4d6a71881f',
+    storageBucket: 'pushserbia-be79a.firebasestorage.app',
+    apiKey: 'AIzaSyCduiH8Da5hvBUn84_Ddl7bNoRmTVSVsQg',
+    authDomain: 'pushserbia-be79a.firebaseapp.com',
+    messagingSenderId: '245366071118',
+    measurementId: 'G-ZHFBEDQBG5',
+  },
 };
