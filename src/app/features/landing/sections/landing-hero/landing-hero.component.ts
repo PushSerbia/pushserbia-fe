50529724604165c3bc0deaf085bea0ed2307a796
Dusan Perisic <dullle@live.com>
2025-11-12 18:59:06 +0100

    chore: update landing hero component with rich member data model and improve HTML formatting

--- src/app/features/landing/sections/landing-hero/landing-hero.component.ts
+++ src/app/features/landing/sections/landing-hero/landing-hero.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+interface Member {
+  name: string;
+  initials: string;
+  imageUrl: string;
+  linkedinUrl: string;
+}
+
 @Component({
   selector: 'app-landing-hero',
   imports: [RouterLink],
@@ -8,5 +15,24 @@ import { RouterLink } from '@angular/router';
   styleUrl: './landing-hero.component.css',
 })
 export class LandingHeroComponent {
-  members = ['MK', 'SV', 'MM', 'DP'];
+  members: Member[] = [
+    {
+      name: 'Miloš Krstić',
+      initials: 'MK',
+      imageUrl: '',
+      linkedinUrl: '',
+    },
+    {
+      name: 'Marko Makarić',
+      initials: 'MM',
+      imageUrl: '',
+      linkedinUrl: '',
+    },
+    {
+      name: 'Dušan Perišić',
+      initials: 'DP',
+      imageUrl: '/images/users/dusan-perisic.jpg',
+      linkedinUrl: 'https://www.linkedin.com/in/dusan-perisic-672772202/',
+    },
+  ];
 }
