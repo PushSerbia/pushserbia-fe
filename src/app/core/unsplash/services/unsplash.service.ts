b1b1603759017215dc9e9ec2c90a207c3aced6d5
Dusan Perisic <dullle@live.com>
2025-12-27 15:07:39 +0100

    refactor: improve state handling in vote store, switch project fetch to paginated response, and refactor Unsplash service to use HttpClient injection instead of ApiService inheritance

--- src/app/core/unsplash/services/unsplash.service.ts
+++ src/app/core/unsplash/services/unsplash.service.ts
@@ -1,28 +1,21 @@
-import { Injectable } from '@angular/core';
-import { ApiService } from '../../api/api.service';
+import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { UnsplashPhoto, UnsplashSearchResponse } from '../interfaces/unsplash-photo.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UnsplashService extends ApiService<UnsplashPhoto> {
-  protected override endpoint = 'unsplash';
-
-  override getAll(): Observable<UnsplashPhoto[]> {
-    return this.httpClient.get<UnsplashSearchResponse>(`/${this.endpoint}`)
-      .pipe(
-        map(this.mapSearchResponseToResults.bind(this))
-      );
-  }
+export class UnsplashService {
+  private httpClient = inject(HttpClient);
+  private endpoint = 'unsplash';
 
   searchPhotos(query: string): Observable<UnsplashPhoto[]> {
-    return this.httpClient.get<UnsplashSearchResponse>(`/${this.endpoint}/search`, {
-      params: { query },
-    })
-    .pipe(
-      map(this.mapSearchResponseToResults.bind(this))
-    );
+    return this.httpClient
+      .get<UnsplashSearchResponse>(`/${this.endpoint}/search`, {
+        params: { query },
+      })
+      .pipe(map(this.mapSearchResponseToResults.bind(this)));
   }
 
   private mapSearchResponseToResults(response: UnsplashSearchResponse): UnsplashPhoto[] {
