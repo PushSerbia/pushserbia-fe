a2922a65cab7d2179c12267bc91013ffe7800e12
Milos Krstic <mrkrsticmilos@gmail.com>
2026-01-06 19:45:55 +0100

    Unsplash URL Formatter Pipe

---
+++ src/app/shared/unsplash-url-formatter.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+/*
+  &w=1080
+  &h=720
+  &fm=jpg
+  &q=80
+*/
+@Pipe({
+  name: 'unsplashUrlFormatter'
+})
+export class UnsplashUrlFormatterPipe implements PipeTransform {
+  transform(unsplashBaseUrl: string, dimensions: [number, number]): string {
+    const crop = 'entropy';
+    const [width, height] = dimensions;
+    const quality = 80;
+    const format = 'webp';
+
+    return `${unsplashBaseUrl}?crop=${crop}&w=${width}&h=${height}&fm=${format}&q=${quality}`;
+  }
+}
