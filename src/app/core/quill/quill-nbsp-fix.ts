1768a3f3295041511c9f0aa9cd70f17c5cf1f460
Milos Krstic <mrkrsticmilos@gmail.com>
2026-01-09 13:17:15 +0100

    Quill Nbsp Fix Type

--- src/app/core/quill/quill-nbsp-fix.ts
+++ src/app/core/quill/quill-nbsp-fix.ts
@@ -1,11 +1,16 @@
 // https://github.com/slab/quill/issues/4535#issuecomment-2838716249
 
+import Quill from "quill";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function quillNbspFix(quill: any): void {
+type QuillWithSemanticHTML = Quill & {
+  legacyGetSemanticHTML?: typeof Quill.prototype.getSemanticHTML;
+  getSemanticHTML: typeof Quill.prototype.getSemanticHTML;
+};
+
+export function quillNbspFix(quill: QuillWithSemanticHTML): void {
   quill.legacyGetSemanticHTML = quill.getSemanticHTML;
-  quill.getSemanticHTML = (a: number, b: number) =>
+  quill.getSemanticHTML = ((a: number, b: number) =>
     quill
-      .legacyGetSemanticHTML(a, b)
-      .replaceAll(/((?:&nbsp;)*)&nbsp;/g, '$1 ');
+      .legacyGetSemanticHTML!(a, b)
+      .replaceAll(/((?:&nbsp;)*)&nbsp;/g, '$1 ')) as typeof Quill.prototype.getSemanticHTML;
 }
